11000c6266a93c99cd6cec7c8acdcb2f29d9306e
Luiz Flávio Rodrigues Lopes (SEPLAG) <m752818@ca.intra>
2019-06-05 20:02:58 -0300

    api

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken, renewToken } from "./Auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  //baseURL: "http://localhost:5000",
   headers: { "Content-Type": "application/json" }
 });
 
